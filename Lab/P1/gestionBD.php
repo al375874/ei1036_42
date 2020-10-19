c9aa9f1aea8c01718c9927c832adebe9716cc20d
al375874 <al375874@uji.es>
2020-10-19 17:40:25 +0200

    master

---
+++ Lab/P1/gestionBD.php
@@ -0,0 +1,3 @@
+<?php
+    $pdo = new PDO("pgsql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+?>
--- Lab/P1/partials/registerForm.php
+++ Lab/P1/partials/registerForm.php
@@ -3,19 +3,34 @@
 	<form class="fom_usuario" action="?action=registrar" method="POST">
 
 		<legend>Datos básicos</legend>
-		<label for="nombre">Nombre</label>
+		<label for="name">Nombre</label>
 		<br/>
-		<input type="text" name="userName" class="item_requerid" size="20" maxlength="25" value="<?php print $userName ?>"
-		 placeholder="Miguel Cervantes" />
+		<input type="text" name="name" class="item_requerid" size="20" maxlength="50" value="<?php print $name ?>"
+		 />
 		<br/>
-		<label for="email">Email</label>
+		<label for="surname">Apellido</label>
 		<br/>
-		<input type="text" name="email" class="item_requerid" size="20" maxlength="25" value="<?php print $email ?>"
-		 placeholder="kiko@ic.es" />
+		<input type="text" name="surname" class="item_requerid" size="20" maxlength="50" value="<?php print $surname ?>"
+		 />
 		<br/>
-		<label for="passwd">Clave</label>
+		<label for="address">Direccion</label>
 		<br/>
-		<input type="password" name="passwd" class="item_requerid" size="8" maxlength="25" value="<?php print $passwd ?>"
+		<input type="text" name="address" class="item_requerid" size="8" maxlength="50" value="<?php print $address ?>"
+		/>
+		<br/>
+		<label for="city">Ciudad</label>
+		<br/>
+		<input type="text" name="city" class="item_requerid" size="8" maxlength="50" value="<?php print $city ?>"
+		/>
+		<br/>
+		<label for="zip_code">CP</label>
+		<br/>
+		<input type="text" name="zip_code" class="item_requerid" size="5" maxlength="5" value="<?php print $zip_code ?>"
+		/>
+		<br/>
+		<label for="foto_file">Foto web</label>
+		<br/>
+		<input type="text" name="foto_file" class="item_requerid" size="8" maxlength="25" value="<?php print $foto_file ?>"
 		/>
 		<br/>
 		<p><input type="submit" value="Enviar">
